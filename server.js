5e8d145c06d875babb43732555193e274a4e739b
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-18 11:12:47 +0000

    first deployment

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+'use strict';
+
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, DATABASE_URL } = require('./config');
+
+// For later:
+// const { Recipient } = require('./models');
+
 const app = express();
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+let server;
+let port = PORT;
+server = app.listen(port, () => {
+	console.log(`Your app is listening on ${port}`);
+});
