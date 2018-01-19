18587112f5f767704142961ad1b9ae214765c484
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-19 10:41:51 +0000

    setup mock async page

--- public/app.js
+++ public/app.js
@@ -1,11 +1,38 @@
 'use strict';
 
-let MOCK_GIFT_LIST = {
-	"name": "Steve"
+let MOCK_GIFT_LISTS = {
+	"giftLists": [{
+		"name": "Steve",
+		"gift": "Plant pot"
+	}, {
+		"name": "Mike",
+		"gift": "Cheese hamper"
+	}, {
+		"name": "Mel",
+		"gift": "Air freshener"
+	}]
 };
 
-function getGiftLists() {
-	$('p').text(MOCK_GIFT_LIST.name);	
+function getGiftListsData() {
+	return MOCK_GIFT_LISTS;
 }
 
-getGiftLists();
+function createGiftListsHtml(data) {
+	let giftLists = data.giftLists;
+	let giftListsHtml;
+	giftLists.forEach(giftList => {
+		giftListsHtml += `
+		<h2>${giftList.name}</h2>
+		<p>${giftList.gift}</p>
+		`
+	})
+	return giftListsHtml;
+}
+
+function showGiftLists() {
+	let giftListsData = getGiftListsData();
+	let giftListsHtml = createGiftListsHtml(giftListsData);
+	$('.js-gift-lists').html(giftListsHtml);
+}
+
+showGiftLists();
