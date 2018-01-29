071912ce384dcf8cac799e7ab72d19bb3a927761
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-29 09:03:43 +0000

    eslint

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-'use strict'
-
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gift-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-gift-app';
 exports.PORT = process.env.PORT || 5000;
