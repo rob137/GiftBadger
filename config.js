d6e4e0a53d01493e3a4b6644de017703ed4fadc3
Robert Kirby <robertaxelkirby@gmail.com>
2018-01-30 16:37:02 +0000

    dependencies for jwt

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/gift-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-gift-app';
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost/gift-app';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost/test-gift-app';
 exports.PORT = process.env.PORT || 5000;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
