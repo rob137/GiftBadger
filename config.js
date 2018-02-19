8bb541ac04594c317f757512b9b7e36bb351fce8
Robert Kirby <robertaxelkirby@gmail.com>
2018-02-19 11:09:59 +0000

    removed jwt refs

--- config.js
+++ config.js
@@ -7,5 +7,3 @@ exports.TEST_DATABASE_URL =
   global.DATABASE_URL ||
   'mongodb://localhost/test-gift-app';
 exports.PORT = process.env.PORT || 5000;
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
